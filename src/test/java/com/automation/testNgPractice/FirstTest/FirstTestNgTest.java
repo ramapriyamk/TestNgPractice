60424f3e0521c2e8895919e31782812c6457d6d3
Ramapriya MK <mkramapriya4@gmail.com>
2020-05-17 19:37:06 -0500

    First Commit with Just @test annotation

---
+++ src/test/java/com/automation/testNgPractice/FirstTest/FirstTestNgTest.java
@@ -0,0 +1,11 @@
+package com.automation.testNgPractice.FirstTest;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNgTest {
+ 
+@Test
+  public void firstTest() {
+		System.out.println("This is my First TestNG Test");
+  }
+}
